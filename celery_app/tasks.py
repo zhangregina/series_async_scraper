cbf52e24247c656328a1905f3c2189cc227b0004
Regina Zhang <zhangregina28@gmail.com>
2023-02-02 00:05:33 +0600

    added scraper launching func in celery-beat

--- celery_app/tasks.py
+++ celery_app/tasks.py
@@ -1,9 +1,11 @@
+import asyncio
 from django.views.decorators.csrf import csrf_exempt
-
+from app.scraper import RezkaSeriesScraper
 from main.celery import app
 
 
 @app.task
 @csrf_exempt
 def start_scraping():
-    print("hello from celery")
+    scraper = RezkaSeriesScraper()
+    asyncio.run(scraper.main())
